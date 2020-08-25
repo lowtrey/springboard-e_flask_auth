1654c662b1acecb9af47eba0a1b8f7aaf61819db
Q. Price <priceqp@gmail.com>
2020-08-25 17:56:32 -0500

    implement user model and class methods for auth

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, redirect, session, flash
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db
+from models import db, connect_db, User
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgres:///auth_demo"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///auth_demo"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
 app.config["SECRET_KEY"] = "abc123"
@@ -13,7 +13,9 @@ app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
-db.create_all()
+
+# db.drop_all()
+# db.create_all()
 
 @app.route("/")
 def show_index():
--- models.py
+++ models.py
@@ -1,7 +1,46 @@
 from flask_sqlalchemy import SQLAlchemy 
+from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
 
+bcrypt = Bcrypt()
+
 def connect_db(app):
+  """Connect to database"""
   db.app = app
   db.init_app(app)
+
+
+class User(db.Model):
+
+  __tablename__ = "users"
+
+  id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
+  username = db.Column(db.Text, unique=True, nullable=False)
+
+  password = db.Column(db.Text, nullable=False)
+
+  @classmethod
+  def register(cls, username, password):
+    """Register user with hashed password & return user."""
+
+    hashed = bcrypt.generate_password_hash(password)
+    # Turn bytestring into normal (unicode utf8) string
+    hashed_utf8 = hashed.decode("utf8")
+
+    # Return instance of user with username & hashed password
+    return cls(username=username, password=hashed_utf8)
+
+  @classmethod
+  def authenticate(cls, username, password):
+    """Validate user and password."""
+
+    user = User.query.filter_by(username=username).first()
+    valid_password = bcrypt.check_password_hash(user.password, password)
+
+    if user and valid_password:
+      # Return user instance
+      return user
+    else:
+      return False
