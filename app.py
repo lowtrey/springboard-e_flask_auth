74ba0d5cc7c4e87a0ab2d0e534028bdada80cba6
Q. Price <priceqp@gmail.com>
2020-08-25 19:08:30 -0500

    implement user registration route and form

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect, session, flash
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, User
+from forms import UserForm
 
 app = Flask(__name__)
 
@@ -20,3 +21,23 @@ connect_db(app)
 @app.route("/")
 def show_index():
   return render_template("index.html")
+
+@app.route("/tweets")
+def show_tweets():
+  return render_template("tweets.html")
+
+# Registration Route
+@app.route("/register", methods=["GET", "POST"])
+def register_user():
+  form = UserForm()
+  if form.validate_on_submit():
+    username = form.username.data
+    password = form.password.data
+    new_user = User.register(username, password)
+
+    db.session.add(new_user)
+    db.session.commit()
+    flash("Welcome! Successfully Created Your Account!")
+    return redirect("/tweets")
+  else:
+    return render_template("register.html", form=form)
