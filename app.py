2d6bc54607e40b25d00f30651a70691dd672d355
Q. Price <priceqp@gmail.com>
2020-08-25 19:45:05 -0500

    add conditional nav render and save active user to session

--- app.py
+++ app.py
@@ -24,6 +24,9 @@ def show_index():
 
 @app.route("/tweets")
 def show_tweets():
+  if "user_id" not in session:
+    flash("You must login to view this page.")
+    return redirect("/")
   return render_template("tweets.html")
 
 # Registration Route
@@ -37,22 +40,32 @@ def register_user():
 
     db.session.add(new_user)
     db.session.commit()
+    session["user_id"] = new_user.id
     flash("Welcome! Successfully Created Your Account!")
     return redirect("/tweets")
   else:
     return render_template("register.html", form=form)
 
+# Login Route
 @app.route("/login", methods=["GET", "POST"])
 def login_user():
   form = UserForm()
   if form.validate_on_submit():
     username = form.username.data
     password = form.password.data
-
     user = User.authenticate(username, password)
     if user:
+      flash(f"Welcome Back, {user.username}!")
+      session["user_id"] = user.id
       return redirect("/tweets")
     else:
       form.username.errors = ["Invalid username / password."]
-      
+
   return render_template("login.html", form=form)
+
+# Logout Route (Convention is POST request)
+@app.route("/logout")
+def logout_user():
+  session.pop("user_id")
+  flash("Goodbye!")
+  return redirect("/")
